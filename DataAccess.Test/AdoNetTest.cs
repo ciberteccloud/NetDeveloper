52e9ca284283d23da191586f36904db1de44854c
juvega <cevegaju@hotmail.com>
2017-01-17 15:59:07 -0500

    Add project files.

---
+++ DataAccess.Test/AdoNetTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Linq;
+using Models;
+
+namespace DataAccess.Test
+{
+    [TestClass]
+    public class AdoNetTest
+    {
+        private readonly AdoNet _adonet;
+        public AdoNetTest()
+        {
+            _adonet = new AdoNet(ConfigurationManager.ConnectionStrings["ChinookConnection"].ConnectionString);
+        }
+
+        [TestMethod]
+        public void Test_Connection_And_Count_Greater_Than_Zero()
+        {
+            var count = _adonet.Count();
+            Assert.AreEqual(count > 0, true);
+        }
+
+        [TestMethod]
+        public void Search_Artist_By_Id()
+        {
+            var artist = _adonet.GetArtistById(1);
+            var expectedArtist = new Artist
+            {
+                ArtistId = 1,
+                Name = "AC/DC"
+            };
+            Assert.AreEqual(expectedArtist.ArtistId, artist.ArtistId);
+            Assert.AreEqual(expectedArtist.Name, artist.Name);
+        }
+
+        [TestMethod]
+        public void Search_Artist_By_Id_From_Store_Procedure()
+        {
+            var artist = _adonet.GetArtistById_Store_Procedure(1);
+            var expectedArtist = new Artist
+            {
+                ArtistId = 1,
+                Name = "AC/DC"
+            };
+            Assert.AreEqual(expectedArtist.ArtistId, artist.ArtistId);
+            Assert.AreEqual(expectedArtist.Name, artist.Name);
+        }
+
+        [TestMethod]
+        public void Get_List_Of_Artist()
+        {
+            var artistList = _adonet.GetListArtist();            
+            Assert.AreEqual(artistList.Count()>0,true);            
+        }
+
+        [TestMethod]
+        public void Insert_Artist()
+        {
+            var artistId = _adonet.InsertArtist("New Artist");
+            Assert.AreEqual(artistId > 0, true);
+        }
+    }
+}
---
+++ DataAccess/AdoNet.cs
@@ -0,0 +1,114 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class AdoNet
+    {
+        private readonly string _connectionString;
+        public AdoNet(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public int Count()
+        {
+            var query = "SELECT Count(ArtistId) FROM dbo.Artist";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                connection.Open();
+                return (int)command.ExecuteScalar();
+            }
+        }
+
+        public Artist GetArtistById(int id)
+        {
+            var query = $"SELECT TOP 1 ArtistId, Name FROM dbo.Artist WHERE ArtistId={id}";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(query, connection);
+                var reader = command.ExecuteReader();
+                Artist artist= new Artist();
+                while (reader.Read())
+                {
+                    artist.ArtistId = Convert.ToInt32(reader["ArtistId"]);
+                    artist.Name = reader["Name"].ToString();                    
+                }
+                return artist;
+            }
+        }
+
+        public Artist GetArtistById_Store_Procedure(int id)
+        {
+            var storeName = "dbo.ArtistById";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(storeName, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.Add("@artistId", SqlDbType.Int).Value = id;
+
+                var reader = command.ExecuteReader();
+                Artist artist = new Artist();
+                while (reader.Read())
+                {
+                    artist.ArtistId = Convert.ToInt32(reader["ArtistId"]);
+                    artist.Name = reader["Name"].ToString();
+                }
+                return artist;
+            }
+        }
+
+        public IEnumerable<Artist> GetListArtist()
+        {
+            var storeName = "dbo.GetListOfArtist";
+            var artistList = new List<Artist>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(storeName, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };                
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    artistList.Add(new Artist
+                    {
+                        ArtistId = Convert.ToInt32(reader["ArtistId"]),
+                        Name = reader["Name"].ToString()
+                    });
+                }
+                return artistList;
+            }
+        }
+        public int InsertArtist(string name)
+        {
+            var storeName = "dbo.InsertArtist";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(storeName, connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = name;
+
+                var artistId = (int)command.ExecuteScalar();                
+                return artistId;
+            }
+        }
+    }
+}
