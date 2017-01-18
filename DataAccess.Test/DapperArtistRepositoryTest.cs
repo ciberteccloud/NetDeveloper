0889d5a3a6f0a436e7154ebc019996990133c1d0
juvega <cevegaju@hotmail.com>
2017-01-17 23:42:55 -0500

    Dapper, migrando métodos de ADO Net.

---
+++ DataAccess.Test/DapperArtistRepositoryTest.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.DapperRepo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace DataAccess.Test
+{
+    [TestClass]
+    public class DapperArtistRepositoryTest
+    {
+        private readonly ArtistRepository _dapperRepository;
+        public DapperArtistRepositoryTest()
+        {
+            _dapperRepository = new ArtistRepository(ConfigurationManager.ConnectionStrings["ChinookConnection"].ConnectionString);
+        }
+
+        [TestMethod]
+        public void Test_Connection_And_Count_Greater_Than_Zero()
+        {
+            var count = _dapperRepository.Count();
+            Assert.AreEqual(count > 0, true);
+        }
+
+        [TestMethod]
+        public void Search_Artist_By_Id()
+        {
+            var artist = _dapperRepository.GetArtistById(1);
+            var expectedArtist = new Artist
+            {
+                ArtistId = 1,
+                Name = "AC/DC"
+            };
+            Assert.AreEqual(expectedArtist.ArtistId, artist.ArtistId);
+            Assert.AreEqual(expectedArtist.Name, artist.Name);
+        }
+
+        [TestMethod]
+        public void Get_List_Of_Artist()
+        {
+            var artistList = _dapperRepository.GetListArtist();
+            Assert.AreEqual(artistList.Count() > 0, true);
+        }
+
+        [TestMethod]
+        public void Insert_Artist()
+        {
+            var artistId = _dapperRepository.InsertArtist("New Artist Dapper");
+            Assert.AreEqual(artistId > 0, true);
+        }
+
+        [TestMethod]
+        public void Delete_Artist_By_Id()
+        {
+            var artistId = _dapperRepository.DeleteArtistById(279);
+            Assert.AreEqual(279, artistId);
+        }
+    }
+}
---
+++ DataAccess/DapperRepo/ArtistRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using Models;
+using System.Data;
+
+namespace DataAccess.DapperRepo
+{
+    public class ArtistRepository
+    {
+        private readonly string _connectionString;
+        public ArtistRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int Count()
+        {
+            var query = "SELECT Count(ArtistId) FROM dbo.Artist";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var result = connection.QueryFirst<int>(query);
+                return result;
+            }
+        }
+        
+        public Artist GetArtistById(int id)
+        {
+            var storeName = "dbo.ArtistById";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var artist = connection.Query<Artist>(storeName, new { artistId=1 },
+                                                      commandType: CommandType.StoredProcedure
+                                                      ).SingleOrDefault();
+                return artist;
+            }
+        }
+        
+        public IEnumerable<Artist> GetListArtist()
+        {
+            var storeName = "dbo.GetListOfArtist";
+            var artistList = new List<Artist>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var artist = connection.Query<Artist>(storeName,
+                                                      commandType: CommandType.StoredProcedure);
+                return artist;                
+            }
+        }
+        public int InsertArtist(string name)
+        {
+            var storeName = "dbo.InsertArtist";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var artist = connection.Query<int>(storeName,
+                                                   new { Name = name}, 
+                                                   commandType: CommandType.StoredProcedure)
+                                                  .SingleOrDefault();
+                return artist;                
+            }
+        }
+
+        public int DeleteArtistById(int id)
+        {
+            var storeName = "dbo.DeleteArtist";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+
+                var artist = connection.Query<int>(storeName,
+                                                   new { artistId = id },
+                                                   commandType: CommandType.StoredProcedure)
+                                                  .SingleOrDefault();
+                return artist;                
+            }
+        }
+    }
+}
