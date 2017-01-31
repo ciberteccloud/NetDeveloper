184ad326b29a73e041bd17b7dcf94a859820b535
Juvega <juvega@gmail.com>
2017-01-31 09:49:30 -0500

    1. Configuración adecuada de nuestro contexto de datos. 2. Agregamos los DataAnnotations basicos a nuestros modelos. 3. Refactorizamos los modelos para permitir relacionarse con otras entidades. 4. Configuramos el Datacontext para indicar el como se relacionan las entidades que se refactorizaron.

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -1,8 +1,22 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Models
 {
-    public  class Genre
+    [Table("Genre")]
+    public partial class Genre
     {
+        public Genre()
+        {
+            Tracks = new HashSet<Track>();
+        }
+
         public int GenreId { get; set; }
-        public string Name { get; set; }        
+
+        [StringLength(120)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Track> Tracks { get; set; }
     }
 }
--- Models/Playlist.cs
+++ Models/Playlist.cs
@@ -1,14 +1,22 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
 {
-    public class Playlist
+    [Table("Playlist")]
+    public partial class Playlist
     {
+        public Playlist()
+        {
+            Tracks = new HashSet<Track>();
+        }
+
         public int PlaylistId { get; set; }
+
+        [StringLength(120)]
         public string Name { get; set; }
+
+        public virtual ICollection<Track> Tracks { get; set; }
     }
 }
