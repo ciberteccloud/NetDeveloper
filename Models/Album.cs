58c5b243a44bc3962442e1f5ad8ee9395500d399
Juvega <juvega@gmail.com>
2017-02-02 13:42:05 -0500

    Agregando la clase album.

---
+++ Models/Album.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace Models
+{    
+    public partial class Album
+    {
+        public int AlbumId { get; set; }
+         
+        public string Title { get; set; }
+
+        public int ArtistId { get; set; }        
+    }
+}
