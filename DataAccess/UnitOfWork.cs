0b9b4ff015dce5ed0f3cf3cfcf871cf4f447b4db
Juvega <juvega@gmail.com>
2017-02-06 13:59:05 -0500

    Crear los repositorios de todas nuestras clases.

--- DataAccess/UnitOfWork.cs
+++ DataAccess/UnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Repositories;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +15,28 @@ namespace DataAccess
         public UnitOfWork(ChinookContext context)
         {
             _context = context;
-            Artists = new ArtistRepository(_context);            
+            Artists = new ArtistRepository(_context);
+            Albums = new Repository<Album>(_context);
+            Customers = new Repository<Customer>(_context);
+            Employees = new Repository<Employee>(_context);
+            Genres = new Repository<Genre>(_context);
+            Invoices = new Repository<Invoice>(_context);
+            InvoiceLines = new Repository<InvoiceLine>(_context);
+            MediaTypes = new Repository<MediaType>(_context);
+            Playlists = new Repository<Playlist>(_context);
+            Tracks = new Repository<Track>(_context);
         }
 
-        public IArtistRepository Artists { get; private set; }        
+        public IArtistRepository Artists { get; private set; }
+        public Repository<Album> Albums { get; private set; }
+        public Repository<Customer> Customers { get; private set; }
+        public Repository<Employee> Employees { get; private set; }
+        public Repository<Genre> Genres { get; private set; }
+        public Repository<Invoice> Invoices { get; private set; }
+        public Repository<InvoiceLine> InvoiceLines { get; private set; }
+        public Repository<MediaType> MediaTypes { get; private set; }
+        public Repository<Playlist> Playlists { get; private set; }
+        public Repository<Track> Tracks { get; private set; }
 
         public int Complete()
         {
