aef1fbc943b0a89d88b5955281e287ddeddc6022
juvega <cevegaju@hotmail.com>
2017-02-16 01:49:15 -0500

    Logueo de informacion del numero de usuarios conectados.

--- WebForms/Global.asax.cs
+++ WebForms/Global.asax.cs
@@ -14,25 +14,45 @@ namespace WebForms
     public class Global : HttpApplication
     {
         private ILog _logger;
+        public Global()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            _logger = LogManager.GetLogger(typeof(Global));
+        }
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            log4net.Config.XmlConfigurator.Configure();
-            _logger = LogManager.GetLogger(typeof(Global));
+            BundleConfig.RegisterBundles(BundleTable.Bundles);            
             _logger.Debug("Logging is enabled");
+            Application["Users"] = 0;
         }
 
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Application["Users"] = (int)Application["Users"] + 1;
+            NumberOfUsers();
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {            
+            Application["Users"] = Convert.ToInt32(Application["Users"])-1;
+            NumberOfUsers();
+        }
+        
         protected void Application_Error(Object sender, EventArgs e)
         {   
             Exception ex = Server.GetLastError();
             if (ex is ThreadAbortException)
-                return;
-            _logger = LogManager.GetLogger(typeof(Global));
+                return;            
             _logger.Error(ex);
             Response.Redirect("Error.aspx");
         }
+
+        private void NumberOfUsers()
+        {
+            _logger.Info($"Number of users connected {Application["Users"]}");
+        }
     }
+
 }
